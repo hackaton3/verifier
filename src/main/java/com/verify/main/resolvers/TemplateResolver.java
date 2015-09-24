9fda198bdac36253bc6fec8f0f34b7506b598a1f
zhiweihuang3388 <zhiwei.huang3388@gmail.com>
2015-09-25 00:44:34 +0800

    Completed the basic logic for loading actual template info.

--- src/main/java/com/verify/main/resolvers/TemplateResolver.java
+++ src/main/java/com/verify/main/resolvers/TemplateResolver.java
@@ -1,5 +1,6 @@
 package com.verify.main.resolvers;
 
+import java.io.File;
 import java.io.IOException;
 import java.math.BigDecimal;
 import java.util.ArrayList;
@@ -8,10 +9,13 @@ import java.util.Map;
 
 import javax.sql.DataSource;
 
+import org.apache.commons.io.FileUtils;
 import org.postgresql.ds.PGPoolingDataSource;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import com.verify.main.util.GsonUtil;
+import com.verify.main.util.SSHUtil;
+import com.verify.main.util.XpathUtil;
 import com.verify.main.verifyobjs.Template;
 
 
@@ -44,6 +48,7 @@ public class TemplateResolver {
 		
 	}
 	
+	//TODO Need to figure out the uncertain path and xpath expression
 	public List<Template> resolveExpectedTemplate(String rootPath) throws IOException{
 		String jsonConfigPath = rootPath + JSON_PATH;
 		String templatePath = rootPath + TEMPLATE_RPM_PATH;
@@ -53,14 +58,36 @@ public class TemplateResolver {
 		@SuppressWarnings("unchecked")
 		List<String> rpmList =  (ArrayList<String>) jsonMap.get("templateRpms");
 		
-		String rpmPath = "";
+		String unpackRpmCommand = "cd %s; rpm2cpio %s | cpio -div";
+		String moveToTmp = "mv -r %s /tmp";
+		String unpackParCommand = "cd /tmp/opt/t...; unzip xx.par";
+		
+		SSHUtil util = new SSHUtil();
+		util.setHost("localhost");
+		util.setUser("root");
+		util.setPassword("root1234");
+		
+		List<Template> expectedList = new ArrayList<Template>();
 		
 		for(String templateName : rpmList){
+			unpackRpmCommand = String.format(unpackRpmCommand, templatePath,  templateName);
+			moveToTmp = String.format(moveToTmp, templatePath);
+			util.runCommand(unpackRpmCommand);
+			util.runCommand(moveToTmp);
+			util.runCommand(unpackParCommand);
 			
+			Template tempalte = new Template();
+			XpathUtil xpathUtil = new XpathUtil("/tmp/opt/.../xxx.jpdl");
+			tempalte.setVersion(xpathUtil.getValue("//xxx/version").get(0));
+			tempalte.setName(xpathUtil.getValue("//xxx/name").get(0));
+			tempalte.setSelectorKey(xpathUtil.getValue("//xxx/selectKey").get(0));
+			expectedList.add(tempalte);
+			File dirToBeDel = new File("/tmp/opt");
+			
+			FileUtils.deleteDirectory(dirToBeDel);
 		}
 		
-		
-		return null;
+		return expectedList;
 	}
 	
 	private DataSource getDataSource(String ip, String dataBaseName,  String userName, String password){
