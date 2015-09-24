2b1786c4cadaf95f1b9315161ec193b2fe7223af
Simon <menghot@gmail.com>
2015-09-24 21:14:16 +0800

    Resource

--- src/main/java/com/verify/main/resolvers/HostResolver.java
+++ src/main/java/com/verify/main/resolvers/HostResolver.java
@@ -12,7 +12,6 @@ import java.util.List;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
-import org.apache.commons.lang3.StringUtils;
 
 import com.verify.main.util.SSHUtil;
 import com.verify.main.verifyobjs.Host;
@@ -24,11 +23,17 @@ import com.verify.main.verifyobjs.Host;
 public class HostResolver {
 
 	public static String IP_RGX = "^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$";
-
 	
+	/**
+	 * 
+	 * @param ip
+	 * @param user
+	 * @param passwd
+	 * @return
+	 */
 	public static List<Host> resoveRemoteHost(String ip, String user, String passwd) {
 		List<Host> hosts = new ArrayList<Host>();
-		String tempFile = System.getProperty("java.io.tmpdir") + System.nanoTime() + "-etc-hosts"; 
+		String tempFile = System.getProperty("java.io.tmpdir") + "etc-hosts-" + System.nanoTime() ; 
 		SSHUtil sshClient = new SSHUtil(); 
 		sshClient.setHost(ip);
 		sshClient.setUser(user);
@@ -37,7 +42,6 @@ public class HostResolver {
 			sshClient.getFile("/etc/hosts", tempFile);
 			return resoveHost(tempFile); 
 		} catch (Exception e) {
-			//FIXME:
 			e.printStackTrace();
 		} finally {
 			FileUtils.deleteQuietly(new File(tempFile)); 
@@ -45,7 +49,11 @@ public class HostResolver {
 		return hosts; 
 	}
 	
-	
+	/**
+	 * 
+	 * @param file
+	 * @return
+	 */
 	public static List<Host> resoveHost(String file) {
 		List<Host> hosts = new ArrayList<Host>();
 		InputStream in = null; 
@@ -53,18 +61,14 @@ public class HostResolver {
 			in = new FileInputStream(new File(file));
 			List<String> lines = IOUtils.readLines(in);
 			for (String line : lines) {
-				if (StringUtils.isNotBlank(line)) {
-					String[] strs = line.trim().split("(\\s)+");
-					if (strs.length > 1 && strs[0].matches(IP_RGX)) {
-						for (int i = 1; i < strs.length; i++) {
-							hosts.add(new Host(strs[i]));
-							System.out.println(strs[i]);
-						}
+				String[] strs = line.trim().split("(\\s)+");
+				if (strs.length > 1 && strs[0].matches(IP_RGX)) {
+					for (int i = 1; i < strs.length; i++) {
+						hosts.add(new Host(strs[i]));
 					}
 				}
 			}
 		} catch (IOException e) {
-			//FIXME
 			e.printStackTrace();
 		} finally {
 			IOUtils.closeQuietly(in);
