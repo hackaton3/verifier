47219d75daa5ea8a84ab532893299fa884265ee4
will8ug <yydragon.000@gmail.com>
2015-09-25 01:44:54 +0800

    Improve toString() & equals() for verification objects.

--- src/main/java/com/verify/main/verifyobjs/AbsBasicVerifyObject.java
+++ src/main/java/com/verify/main/verifyobjs/AbsBasicVerifyObject.java
@@ -2,13 +2,20 @@ package com.verify.main.verifyobjs;
 
 import java.io.Serializable;
 
+import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
 
 public abstract class AbsBasicVerifyObject implements Serializable {
     private static final long serialVersionUID = -7674647537561888177L;
 
     @Override
     public String toString() {
-        return ToStringBuilder.reflectionToString(this);
+        return ToStringBuilder.reflectionToString(this, ToStringStyle.SHORT_PREFIX_STYLE);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return EqualsBuilder.reflectionEquals(this, obj);
     }
 }
